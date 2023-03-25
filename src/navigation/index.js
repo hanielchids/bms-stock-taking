95cac84e0afc357189f65671caa1518aaa8b51ce
hanielchids <hanielchids@gmail.com>
2023-03-25 10:01:09 +0200

    Updated routes

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -7,7 +7,7 @@ import SignInScreen from "../screens/SignInScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import ConfirmEmailScreen from "../screens/ConfirmEmailScreen";
 import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
-// import NewPasswordScreen from "../screens/NewPasswordScreen";
+import NewPasswordScreen from "../screens/NewPasswordScreen";
 // import HomeScreen from "../screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
@@ -20,9 +20,9 @@ const Navigation = () => {
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-        {/* <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
+        <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
 
-        <Stack.Screen name="Home" component={HomeScreen} /> */}
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
