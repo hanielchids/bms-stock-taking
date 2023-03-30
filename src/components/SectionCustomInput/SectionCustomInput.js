41857243ba5c102596795d71f97d4bb628b1cd9d
hanielchids <hanielchids@gmail.com>
2023-03-30 11:47:15 +0200

    created custom input

---
+++ src/components/SectionCustomInput/SectionCustomInput.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { Controller } from "react-hook-form";
+
+const SectionCustomInput = ({
+  control,
+  name,
+  rules = {},
+  placeholder,
+  secureTextEntry,
+}) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({
+        field: { value, onChange, onBlur },
+        fieldState: { error },
+      }) => (
+        <>
+          <View
+            style={[
+              styles.container,
+              { borderColor: error ? "red" : "#e8e8e8" },
+            ]}
+          >
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              placeholder={placeholder}
+              style={styles.input}
+              secureTextEntry={secureTextEntry}
+            />
+          </View>
+          {error && (
+            <Text style={{ color: "red", alignSelf: "stretch" }}>
+              {error.message || "Error"}
+            </Text>
+          )}
+        </>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#A0A0A0",
+    width: "49%",
+    color: "#0D0D0D",
+
+    borderColor: "#e8e8e8",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    marginVertical: 5,
+    marginRight: 5,
+  },
+  input: {},
+});
+
+export default SectionCustomInput;
